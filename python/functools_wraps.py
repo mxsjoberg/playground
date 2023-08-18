f79c557ccce1379f75246d800bc34fe051f60f02
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-08-18 23:19:44 +0200

    Create functools_wraps.py

---
+++ python/functools_wraps.py
@@ -0,0 +1,14 @@
+from functools import wraps
+
+def logging(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"call to {func.__name__}, args: {args}, kwargs: {kwargs}")
+        return func(*args, **kwargs)
+    return wrapper
+
+@logging
+def power(base, exp): return base ** exp
+
+power(2, 3)
+# call to power, args: (2, 3), kwargs: {}
