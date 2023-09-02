6fd879e18f4ab40ae5aae384a8c2355742253156
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-09-02 21:41:19 +0200

    Create while-test.js

---
+++ js/eva_interpreter/__tests__/while-test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+
+module.exports = eva => {
+    assert.strictEqual(eva.eval(
+        ['begin',
+            ['var', 'counter', 0],
+            ['var', 'result', 0],
+            ['while', ['<', 'counter', 10],
+                ['begin',
+                    ['set', 'result', ['+', 'result', 1]],
+                    ['set', 'counter', ['+', 'counter', 1]],
+                ],
+            ],
+            'result'
+        ]),
+    10);
+}
