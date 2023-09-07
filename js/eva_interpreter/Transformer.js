c9bc78ebe79c4fd4026eb16f3b5158e80e59d9c0
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-09-07 22:59:19 +0200

    Create Transformer.js

---
+++ js/eva_interpreter/Transformer.js
@@ -0,0 +1,42 @@
+/*
+
+    AST transformer.
+
+*/
+
+class Transformer {
+
+    // Transform 'def'-expression into variable declaration with lambda expression 
+
+    transformDefToVarLambda(exp) {
+        const [_tag, name, params, body] = exp;
+        return ["var", name, ["lambda", params, body]];
+    }
+
+    // Transform 'switch'-expression to nested 'if'-expressions
+
+    transformSwitchToIf(exp) {
+        const [_tag, ...cases] = exp;
+        
+        const ifExp = ["if", null, null, null];
+        let current = ifExp;
+        
+        for (let i = 0; i < cases.length - 1; i++) {
+            const [currentCond, currentBlock] = cases[i];
+
+            current[1] = currentCond;
+            current[2] = currentBlock;
+
+            const next = cases[i + 1];
+            const [nextCond, nextBlock] = next;
+
+            current[3] = nextCond === 'else' ? nextBlock : ['if'];
+
+            current = current[3];
+        }
+
+        return ifExp;
+    }
+}
+
+module.exports = Transformer;
