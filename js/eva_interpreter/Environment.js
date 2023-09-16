0aa8abe3fd9071f5ede926edc1d049531095a0e1
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-09-16 20:44:03 +0200

    Update Environment.js

--- js/eva_interpreter/Environment.js
+++ js/eva_interpreter/Environment.js
@@ -1,34 +1,36 @@
 /*
-
     Environment for Eva interpreter
-
 */
 
 class Environment {
 
-    // Constructor for record
-    
+    /*
+        Constructor for record
+    */
     constructor(record = {}, parent = null) {
         this.record = record;
         this.parent = parent;
     }
 
-    // Create variable with name and value
-    
+    /*
+        Create variable with name and value
+    */
     define(name, value) {
         this.record[name] = value;
         return value;
     }
 
-    // Update existing variable
-
+    /*
+        Update existing variable
+    */
     assign(name, value) {
         this.resolve(name).record[name] = value;
         return value;
     }
 
-    // Return value of defined variable
-
+    /*
+        Return value of defined variable
+    */
     lookup(name) {
         // if (!this.record.hasOwnProperty(name)) {
         //     throw new ReferenceError(`variable "${name}" is not defined`);
@@ -37,8 +39,9 @@ class Environment {
         return this.resolve(name).record[name];
     }
 
-    // Identifier resolution: return environment where variable is defined (scope chain traversal)
-
+    /*
+        Identifier resolution - return environment where variable is defined (scope chain traversal)
+    */
     resolve(name) {
         if (this.record.hasOwnProperty(name)) {
             return this;
