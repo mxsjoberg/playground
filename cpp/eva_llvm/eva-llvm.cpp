48a17b76856d6d22e708b080cc5cac972085affd
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-11-22 17:57:26 +0800

    Update eva-llvm.cpp

--- cpp/eva_llvm/eva-llvm.cpp
+++ cpp/eva_llvm/eva-llvm.cpp
@@ -5,8 +5,10 @@
 int main(int argc, char const *argv[]) {
     // example program
     std::string program = R"(42)";
+    
     // compiler instance
     EvaLLVM vm;
+    
     // generate LLVM IR
     vm.exec(program);
 }
